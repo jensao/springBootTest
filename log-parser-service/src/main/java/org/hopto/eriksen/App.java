d7d6f33f093e91bc159c2326e95b768538a22354
Jens Eriksen <jens.eriksen@omegapoint.se>
2017-01-24 07:28:35 +0100

    Some small configuration changes

--- log-parser-service/src/main/java/org/hopto/eriksen/App.java
+++ log-parser-service/src/main/java/org/hopto/eriksen/App.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
-// @EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class App {
 
